2de1b4a6f05b1ba6c651f8eeb9d66bfb4d0ac628
Divyam Khanna <divyam.dkhanna@gmail.com>
2026-07-15 20:59:42 +0530

    Beautified and removed extra variable and managed overflows and temporal dead zones

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ const insect_moving_legs=document.getElementsByClassName('moving_legs')
 
 
 
+// INSECT CREATOR AT DOUBLECLICK
 let insect_counter=0
-// INSECT CREATOR
-let head_counter=0
 container_1.addEventListener('dblclick',(event)=>{
-    head_counter+=1
     insect_counter+=1
     if (insect_counter<11) {
-        insect_creator(event,head_counter)
+        insect_creator(event,insect_counter)
     }
 })
 
 
+// RANDOM PLACING OF INSECT AT CLICK
 container_1.addEventListener('click',(event)=>{
 
     for (let index = 0; index < insect_list.length; index++) {
@@ -25,15 +24,15 @@ container_1.addEventListener('click',(event)=>{
         let rect=container_1.getBoundingClientRect()
         let width=rect.width
         let height=rect.height
-        console.log(width,height);
-        let random_placing_height=Math.floor(Math.random() * height) + 1;
-        let random_placing_width=Math.floor(Math.random() * width) + 1;
+        let random_placing_height=Math.floor(Math.random() * height) -22;
+        let random_placing_width=Math.floor(Math.random() * width) -34;
         element.style.top=`${random_placing_height}px`
         element.style.left=`${random_placing_width}px`
 
         let movement_interval=setInterval(() => {
             insect_legs_movement()
         }, 1);
+
         setTimeout(() => {
             clearInterval(movement_interval)
         }, 3200);
@@ -42,18 +41,25 @@ container_1.addEventListener('click',(event)=>{
 
 })
 
-// INSECT MOVEMENT APPLIER
+// INSECT MOVEMENT APPLIER AT MOUSEMOVEMENT
 
 let top_setter=0
 let left_setter=0
 let size_push=0
 container_1.addEventListener('mousemove',(event)=>{
     for (let index = 0; index < insect_list.length; index++) {
+        const randomInt=Math.floor(Math.random()*50)+1
+        if (randomInt%2==0) {
+            size_push+=randomInt
+        }
+        else{
+            size_push-=randomInt
+        }
         const element = insect_list[index];
-            element.style.top=`${event.clientY-size_push}px`
-            element.style.left=`${event.clientX-size_push}px`
+        element.style.top=`${event.clientY-size_push+27}px`
+        element.style.left=`${event.clientX+size_push-15}px`
     }
-
+    size_push=0
     insect_legs_movement()
 })
 
@@ -75,9 +81,22 @@ function insect_creator(event,head_count) {
     insect.style.top=`${event.clientY}px`
     insect.style.left=`${event.clientX}px`
     insect.classList.add('insect')
-
+    
     // Insect Head Properties
     insect_head.classList.add('insect_head')
+    const randomInt=Math.floor(Math.random()*4)+1
+    if (randomInt==1) {
+        insect_head.style.background='red'
+    }
+    if (randomInt==2) {
+        insect_head.style.background='green'
+    }
+    if (randomInt==3) {
+        insect_head.style.background='blue'
+    }
+    if (randomInt==4) {
+        insect_head.style.background='yellow'
+    }
     insect_head.innerHTML=`${head_count}`
 
     // Insect Leg Properties
@@ -99,7 +118,7 @@ function insect_creator(event,head_count) {
     insect_leg_6.classList.add('insect_leg','leg_6','moving_legs')
     insect_leg_6.style.transform='rotate(300deg)'
 
-    // Adding of Child Element
+    // Adding of Child Elements
     container_1.appendChild(insect)
 
     insect.appendChild(insect_head)
@@ -120,6 +139,8 @@ function insect_legs_movement() {
     for (const element of insect_moving_legs) {
         let prev_deg=element.style.transform
         let degree_of_movement=5
+
+        // Range of angle 10-99
         if(prev_deg.length==13){
             prev_deg=prev_deg.slice(7,9)
             prev_deg=Number(prev_deg)
@@ -130,7 +151,8 @@ function insect_legs_movement() {
                 element.style.transform=`rotate(${prev_deg-degree_of_movement}deg)`
             }       
         }
-
+        
+        // Range of angle 100-999
         else if(prev_deg.length==14){
             prev_deg=prev_deg.slice(7,10)
             prev_deg=Number(prev_deg)
@@ -141,7 +163,9 @@ function insect_legs_movement() {
                 element.style.transform=`rotate(${prev_deg-degree_of_movement}deg)`
             }
         }
-
+        
+        
+        // Range of angle 0-9
         else if(prev_deg==12){
             prev_deg=prev_deg.slice(7,8)
             prev_deg=Number(prev_deg)
